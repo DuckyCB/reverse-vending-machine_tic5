66c3e56d2b1cb864b475aac63a63ee0af7304e00
Pato Carrau <patricio.carrau@outlook.com>
2022-11-10 14:32:27 -0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting project setup"
+
+git clone https://github.com/DuckyCB/reverse-vending-machine_tic5
+
+python -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
